1ee9bf3b47b58b41a1a96c5f7ed45cead4eefc8e
Elif <eliiftum@gmail.com>
2024-06-09 16:52:36 +0300

    [DEV][elif.tum][feature/delegates][delegates is created]

---
+++ Mobven-MovieApp/Mobven-MovieApp/Delegates/AppDelegate.swift
@@ -0,0 +1,8 @@
+//
+//  AppDelegate.swift
+//  Mobven-MovieApp
+//
+//  Created by Elif Tum on 9.06.2024.
+//
+
+import Foundation
---
+++ Mobven-MovieApp/Mobven-MovieApp/Delegates/SceneDelegate.swift
@@ -0,0 +1,8 @@
+//
+//  SceneDelegate.swift
+//  Mobven-MovieApp
+//
+//  Created by Elif Tum on 9.06.2024.
+//
+
+import Foundation
