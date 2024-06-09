655367fe5f06713b3c542a64ab829e99cc2262f4
Elif <eliiftum@gmail.com>
2024-06-09 16:42:10 +0300

    [DEV][elif.tum][feature/coordinator][coordinator is created]

---
+++ Mobven-MovieApp/Mobven-MovieApp/Mobven_MovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mobven_MovieAppApp.swift
+//  Mobven-MovieApp
+//
+//  Created by Elif Tum on 9.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct Mobven_MovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
